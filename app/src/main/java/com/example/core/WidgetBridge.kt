c38d2f18903ee62d27571784069c7d0ca2617a9e
sleepop77-jpg <sleepop77@gmail.com>
2026-08-22 12:39:12 +0530

    StudyOS upgrade via Bridge

--- app/src/main/java/com/example/core/WidgetBridge.kt
+++ app/src/main/java/com/example/core/WidgetBridge.kt
@@ -5,6 +5,8 @@ import kotlinx.coroutines.flow.MutableStateFlow
 object WidgetBridge {
     val seconds = MutableStateFlow(25 * 60)
     val running = MutableStateFlow(false)
+    val frame = MutableStateFlow(0)
+    val streak = MutableStateFlow(4)
 }
 
 object NavBridge {
